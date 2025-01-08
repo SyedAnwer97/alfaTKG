a409c033b6c08a684b26b52eac1d295831206d13
anwer <anwersahira786@gmail.com>
2025-01-08 16:44:48 +0530

    Added Software Library Page Layer

---
+++ src/main/java/alfatkg/pages/SoftwareLibrary.java
@@ -0,0 +1,19 @@
+package alfatkg.pages;
+
+import alfatkg.consant.FrameworkConstants;
+import alfatkg.utils.DynamicXpath;
+import org.openqa.selenium.By;
+
+public class SoftwareLibrary extends PageActions {
+
+    private static final String DYNAMIC_LOCATOR = "//*[.='%s']//input";
+
+    private static final By IMAGE_GAIA_PROCESS_NAVI = By.xpath(DynamicXpath.xpath(DYNAMIC_LOCATOR, "GAIA Process NAVI"));
+
+    public GPNHomePage clickGPN() {
+        customClick(IMAGE_GAIA_PROCESS_NAVI);
+        switchToTab(FrameworkConstants.getGPN_TITLE());
+        closeTheTab(FrameworkConstants.getALFADOCK_TITLE());
+        return new GPNHomePage();
+    }
+}
